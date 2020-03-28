127294a2c633e97e9965d69089b3ebd17fc2c06f
skynyrd <anil.surmeli@gmail.com>
2020-03-28 15:49:18 +1100

    adds booklist application

---
+++ booklist/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BookContextProvider from './contexts/BookContext';
+import NavBar from './components/NavBar';
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div className="App">
+      <BookContextProvider>
+        <NavBar />
+        <BookList/>
+      </BookContextProvider>
+    </div>
+  );
+}
+
+export default App;
